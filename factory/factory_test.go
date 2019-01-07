ea45715f677f030786fa2497edc66b77ac0e6207
Xu Qiaolun <jamesxql@gmail.com>
2019-01-07 15:58:15 +0800

    Add local filesystem test case

---
+++ factory/factory_test.go
@@ -0,0 +1,38 @@
+package factory
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreateLocalFilesystem(t *testing.T) {
+	tp := filepath.Join(os.TempDir(), "extfs-factory-test")
+	fs, err := NewFilesystem(fmt.Sprintf("file://%s", tp))
+	require.NoError(t, err)
+	defer fs.Close()
+
+	err = fs.MkdirAll("demo1", os.ModePerm)
+	require.NoError(t, err)
+
+	f, err := fs.OpenFile("demo1/test.txt", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0777)
+	require.NoError(t, err)
+
+	_, err = f.Write([]byte("Hello world"))
+	require.NoError(t, err)
+	err = f.Close()
+	require.NoError(t, err)
+
+	f, err = fs.Open("demo1/test.txt")
+	require.NoError(t, err)
+	defer f.Close()
+
+	data, err := ioutil.ReadAll(f)
+	require.NoError(t, err)
+	assert.Equal(t, "Hello world", string(data))
+}
--- local/local_test.go
+++ local/local_test.go
@@ -7,20 +7,58 @@ SPDX-License-Identifier: Apache-2.0
 package local
 
 import (
-	"io/ioutil"
 	"os"
 	"path/filepath"
 	"testing"
 
+	"github.com/rkcloudchain/extfs"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func TestCreate(t *testing.T) {
-	tp, _ := ioutil.TempDir(os.TempDir(), "extfs-local-test")
+	tp := filepath.Join(os.TempDir(), "extfs-local-test")
 	fs := New(tp)
 
 	f, err := fs.Create("bar/qux")
 	require.NoError(t, err)
+	defer f.Close()
 	assert.Equal(t, filepath.Join(tp, "bar", "qux"), f.Name())
+}
+
+func TestCreateErrCrossedBoundary(t *testing.T) {
+	fs := New("/foo")
+	_, err := fs.Create("../foo")
+	assert.Equal(t, extfs.ErrCrossedBoundary, err)
+}
+
+func TestOpen(t *testing.T) {
+	tp := filepath.Join(os.TempDir(), "extfs-local-test")
+	fs := New(tp)
+
+	f, err := fs.Open("bar/qux")
+	require.NoError(t, err)
+	defer f.Close()
+	assert.Equal(t, filepath.Join(tp, "bar", "qux"), f.Name())
+}
+
+func TestOpenFile(t *testing.T) {
+	tp := filepath.Join(os.TempDir(), "extfs-local-test")
+	fs := New(tp)
+
+	f, err := fs.OpenFile("bar/qux", os.O_RDONLY, 0666)
+	require.NoError(t, err)
+	defer f.Close()
+	assert.Equal(t, filepath.Join(tp, "bar", "qux"), f.Name())
+}
+
+func TestRemove(t *testing.T) {
+	tp := filepath.Join(os.TempDir(), "extfs-local-test")
+	fs := New(tp)
+
+	err := fs.Remove("bar/qux")
+	require.NoError(t, err)
+
+	_, err = fs.Stat("bar/qux")
+	assert.True(t, os.IsNotExist(err))
 }
