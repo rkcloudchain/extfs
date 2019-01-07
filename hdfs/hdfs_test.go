59de83588915e6cfb3ca898829acaef086ab4d4e
Xu Qiaolun <jamesxql@gmail.com>
2019-01-08 05:11:41 +0800

    Add hdfs filesystem test case

---
+++ hdfs/hdfs_test.go
@@ -0,0 +1,24 @@
+package hdfs
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	hadoopNamenode = "localhost:9000"
+)
+
+func TestCreate(t *testing.T) {
+	fs, err := New(hadoopNamenode, "/cloudchain/test1")
+	require.NoError(t, err)
+	defer fs.Close()
+
+	f, err := fs.Create("myfile.txt")
+	require.NoError(t, err)
+	defer f.Close()
+
+	_, err = f.Write([]byte("Hello world"))
+	require.NoError(t, err)
+}
